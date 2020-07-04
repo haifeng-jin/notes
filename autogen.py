f64dd0e24fbc325736e202b43f67a41b92556955
Haifeng Jin <haifengj@google.com>
2020-07-04 22:07:22 +0000

    cf and lc added

--- autogen.py
+++ autogen.py
@@ -24,17 +24,29 @@ def to_name(name):
     return ' '.join(map(lambda s: s.capitalize(), name.split('_')))
 
 def add_dir(path, depth):
+    files = []
+    dirs = []
+    sub_dirs = {}
     for name in os.listdir(path):
         tmp_path = os.path.join(path, name)
         if tmp_path in ignore:
             continue
         if os.path.isdir(tmp_path):
-            lines.append('  ' * (depth + 1) + '- ' + to_name(name) + ':')
-            add_dir(tmp_path, depth + 1)
+            name_line = '  ' * (depth + 1) + '- ' + to_name(name) + ':'
+            dirs.append(name_line)
+            sub_dirs[name_line] = add_dir(tmp_path, depth + 1)
         elif tmp_path.endswith('md'):
-            lines.append(generate_line(tmp_path, depth))
+            files.append(generate_line(tmp_path, depth))
+    ret = []
+    for dir_name in sorted(dirs):
+        ret.append(dir_name)
+        ret += sub_dirs[dir_name]
+    files = sorted(files)
+    if path.endswith('leetcode'):
+        files = sorted(files, key=lambda x: int(x.strip().split(' ')[1].split('.')[0]))
+    return ret + files
 
-add_dir('notes', 0)
+lines += add_dir('notes', 0)
 
 f = open('mkdocs.yml', 'w')
 f.write(content + '\n'.join(lines) + '\n')
